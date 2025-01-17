b4449a0625785b29e942b764075c53e46ebc5440
xZankzo <xzankzo@gmail.com>
2025-01-18 02:45:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeColor').addEventListener('click', function() {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
